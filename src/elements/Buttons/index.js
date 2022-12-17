7b1d3f4b4ad809c670db9950d647382a29b54fa5
Pitri Indrayani <indrayanipitri@gmail.com>
2022-12-17 21:25:56 +0700

    component button part 2

--- src/elements/Buttons/index.js
+++ src/elements/Buttons/index.js
@@ -3,6 +3,33 @@ import propTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export default function Button(){
+    const className = [props.className]
+    if(isPrimary) className.push("btn-primary")
+    if(isLarge) className.push("btn-lg")
+    if(isSmall) className.push("btn-sm")
+    if(isShadow) className.push("btn-shadow")
+    if(isBlock) className.push("btn-block")
+
+    const onClick = () => {
+        if(props.onClick) props.onClick()
+    };
+
+    if (props.type === "link") {
+       if(props.isExternal) {
+        return (
+            <a href={props.href} className={className.join(" ")} target={props.target ? "_blank" : undefined} rel={props.target ? "noreferrer noopener" : undefined} style={props.style} >{props.children}</a>
+        )
+       } else {
+        return (
+            <Link to={props.href} className={className.join(" ")} style={props.style} onClick={onClick}>
+                {props.children}
+            </Link>
+        )
+       }
+    }
+
+
+
     return (
         <>
         </>
@@ -12,6 +39,7 @@ export default function Button(){
 Button.propTypes = {
     type: propTypes.oneOf(["button", "link"]),
     onClick: propTypes.func,
+    href: propTypes.string,
     target: propTypes.string,
     className: propTypes.string,
     isDisabled: propTypes.bool,
@@ -19,6 +47,8 @@ Button.propTypes = {
     isBlock: propTypes.bool,
     isShadow: propTypes.bool,
     isLarge: propTypes.bool,
-    isSmall: propTypes.bool
+    isSmall: propTypes.bool,
+    isPrimary: propTypes.bool,
+    isExternal: propTypes.bool
 }
 
