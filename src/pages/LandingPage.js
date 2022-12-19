997c3c12ea17bd29bd81bd8195e20ba78999bf9c
Pitri Indrayani <indrayanipitri@gmail.com>
2022-12-19 14:07:32 +0700

    create grid container

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero"
 import landingPage from "json/landingPage"
+import MostPicked from "parts/MostPicked";
 
 class LandingPage extends Component {
     render() {
@@ -9,6 +10,8 @@ class LandingPage extends Component {
             <>
             <Header {...this.props}></Header>
             <Hero data={landingPage.hero} />
+            <MostPicked data={landingPage.hero} />
+            
             </>
         )
     }
---
+++ src/parts/MostPicked.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+export default function MostPicked() {
+    return (
+        <section className="container">
+            <h4 className="mb-3"> Most Picked</h4>
+            <div className="container-grid">
+                <div className="item column-4 row-2">
+                    <div className="card">card</div>
+                </div>
+                <div className="item column-4 row-1">
+                    <div className="card">card</div>
+                </div>
+                <div className="item column-4 row-1">
+                    <div className="card">card</div>
+                </div>
+                <div className="item column-4 row-1">
+                    <div className="card">card</div>
+                </div>
+                <div className="item column-4 row-1">
+                    <div className="card">card</div>
+                </div>
+                
+            </div>
+        </section>
+    )
+}
