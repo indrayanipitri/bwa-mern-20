69eba22b1a6d688aec05754b59fd60735676e9f2
Pitri Indrayani <indrayanipitri@gmail.com>
2022-12-19 18:12:10 +0700

    mostpicked styling part 2

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -10,15 +10,15 @@ export default function MostPicked(props) {
                     return (
                         <div className={`item column-4 ${index === 0 ? "row-2" : "row-1"}`} key={index}>
                             <div className="card card-featured">
-                                <div className="tag">{item.price}
+                                <div className="tag">${item.price}
                                 <span className="font-weight-light">per {item.unit}</span>
                                 </div>
 
                                 <figure className="img-wrapper">
-                                    <img src={item.imageUrl} alt={item.name} />
+                                    <img src={item.imageUrl} alt={item.name} className="img-cover" />
                                 </figure>
 
-                                <div className="metta-wrapper">
+                                <div className="meta-wrapper">
                                     <Button type="link" className="streched-link d-block text-white" href={`/properties/${item._id}`}>
                                         <h5>{item.name}</h5>
                                     </Button>
